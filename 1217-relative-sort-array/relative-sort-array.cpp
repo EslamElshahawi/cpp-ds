7e53a8066fe92e60d7cba72df4c44f874717155f
EslamElshahawi <79419183+EslamElshahawi@users.noreply.github.com>
2024-06-11 19:24:33 +0300

    Time: 5 ms (34.15%) | Memory: 10.1 MB (41.93%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> result;
+        unordered_map<int, int> countMap;
+        vector<int> result, remaining;
 
         for(int i = 0; i < arr2.size(); i++){
-            for(int j = 0; j < arr1.size(); j++){
-                if(arr1[j] == arr2[i]){
-                    result.push_back(arr1[j]);
-                    arr1[j] = -1;
-                }
-            }
+            countMap[arr2[i]] = 0;
         }
 
-        sort(arr1.begin(), arr1.end());
         for(int i = 0; i < arr1.size(); i++){
-            if(arr1[i] != -1){
-                result.push_back(arr1[i]);
+            if(countMap.find(arr1[i]) != countMap.end()){
+                countMap[arr1[i]]++;
+            } else {
+                remaining.push_back(arr1[i]);
             }
         }
+        sort(remaining.begin(), remaining.end());
+        for(int i = 0; i < arr2.size(); i++){
+            for(int j = 0; j < countMap[arr2[i]]; j++){
+                result.push_back(arr2[i]);
+            }
+        }
+        for(int i = 0; i < remaining.size(); i++){
+            result.push_back(remaining[i]); 
+        }
+
         return result;
 
 
